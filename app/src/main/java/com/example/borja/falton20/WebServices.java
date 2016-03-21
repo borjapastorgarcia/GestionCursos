3f3ffeba45a96965700ce87a61817b14441eca3d
Borja Pastor Garcia <borjapg616@gmail.com>
2016-03-21 16:59:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/borja/falton20/WebServices.java
@@ -0,0 +1,19 @@
+package com.example.borja.falton20;
+
+/**
+ * Created by borja on 21/03/2016.
+ */
+public class WebServices {
+
+    private final String URL="https....php";
+
+    public static String URL_REGISTRO=".php";
+    public static String URL_LOGIN=".php";
+    public static String URL_GET_FALTAS=".php";
+    public static String URL_GET_TAREAS=".php";
+    public static String URL_GET_IDEAS=".php";
+    public static String URL_GET_EXAMENES=".php";
+    public static String URL_GET_PERFIL=".php";
+    public static String URL_SET_PERFIL=".php";
+
+}
