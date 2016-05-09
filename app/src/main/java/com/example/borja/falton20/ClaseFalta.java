327650f19906cd73a0beef74f18d489ce4172003
Borja Pastor Garcia <borjapg616@gmail.com>
2016-05-09 11:03:29 +0200

    cambios

---
+++ app/src/main/java/com/example/borja/falton20/ClaseFalta.java
@@ -0,0 +1,7 @@
+package com.example.borja.falton20;
+
+/**
+ * Created by borja on 06/05/2016.
+ */
+public class ClaseFalta {
+}
---
+++ app/src/main/java/com/example/borja/falton20/DatosUsuario.java
@@ -0,0 +1,7 @@
+package com.example.borja.falton20;
+
+/**
+ * Created by borja on 08/05/2016.
+ */
+public class DatosUsuario {
+}
